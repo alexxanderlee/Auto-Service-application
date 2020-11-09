3a0f5996ee4aa9a9da62025cc9d5f928a3307ab8
Alexander Lee <alxnder.lee@gmail.com>
2020-11-09 20:45:55 +0600

    fix images path

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,11 +1,11 @@
 import servicesPic1 from './services_pic1.jpg';
 import servicesPic2 from './services_pic2.jpg';
 import servicesPic3 from './services_pic3.jpg';
-import tirePng from './assets/images/tire.png';
-import diskPng from './assets/images/disk.png';
-import accessoriesPng from './assets/images/accessories.png';
-import oilPng from './assets/images/oil.png';
-import cosmeticsPng from './assets/images/cosmetics.png';
-import toolsPng from './assets/images/tools.png';
+import tirePng from './tire.png';
+import diskPng from './disk.png';
+import accessoriesPng from './accessories.png';
+import oilPng from './oil.png';
+import cosmeticsPng from './cosmetics.png';
+import toolsPng from './tools.png';
 
 export { servicesPic1, servicesPic2, servicesPic3, tirePng, diskPng, accessoriesPng, oilPng, cosmeticsPng, toolsPng };
